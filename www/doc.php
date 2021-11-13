4bcde455a9af7752ebbf371716994f5d0af99824
benoc <benoc@alum.mit.edu>
2021-11-13 15:13:45 -0500

    Add doc redirection script (doc.php)

---
+++ www/doc.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+
+<title>Redirect to puzzle doc</title>
+<?php
+
+require('puzzlebosslib.php');
+
+$name = "";
+$puzzleid = "";
+if (isset( $_GET['pname'] ) ) {
+    $name = $_GET['pname'];
+    $apiurl = "/puzzles";
+    $resp = readapi($apiurl);
+    if (!$resp){
+        echo '</head>';
+        echo '<body><br>Error: api fetch of puzzle catalog failed!</br>';
+        echo '</body></html>';
+        exit (2);
+    }
+    foreach (json_decode($resp)->puzzles as $puzzle) {
+        if ($puzzle->name == $name)
+            $puzzleid = $puzzle->id;
+    }
+    if ($puzzleid == "") {
+        echo '</head>';
+        echo '<body><br>Error: api search for puzzle in catalog failed!</br>';
+        echo '</body></html>';
+        exit (2);
+    }
+    $drive_uri = json_decode(readapi("/puzzles/" . $puzzleid . "/drive_uri"))->puzzle->drive_uri;
+    echo '<meta http-equiv="refresh" content="0;URL=' . $drive_uri . '">';
+}
+
+else {
+    echo '<br>Error: No puzzlename parameter (pname) specified.<br>';
+}
+echo "</head><body><br>Redirecting to doc sheet for puzzle" . $name . " id " . $puzzleid . "<br>";
+?>
+</body>
+</html>
