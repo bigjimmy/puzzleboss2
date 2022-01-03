c4b6d5e6e6d2b13f3b69d913044551a42c164a21
Danny Ben-David <dannybd>
2022-01-03 15:39:49 -0500

    Minor cleanup in addpuzzle.php

--- www/addpuzzle.php
+++ www/addpuzzle.php
@@ -4,27 +4,23 @@
 
 require('puzzlebosslib.php');
 
-$name = "";
-
-if (isset( $_GET['submit'] ) ) {
+if (isset($_GET['submit'])) {
     $name = $_GET['name'];
     $round_id = $_GET['round_id'];
     $puzzle_uri = $_GET['puzzle_uri'];
     
     echo 'Attempting to add puzzle.<br>';
-    echo 'name: ' . $name . '<br>';
-    echo 'round_id: ' . $round_id . '<br>';
-    echo 'puzzle_uri: ' . $puzzle_uri . '<br>';
+    echo "name: $name<br>";
+    echo "round_id: $round_id<br>";
+    echo "puzzle_uri: $puzzle_uri<br>";
     
     
     $apiurl = "/puzzles";
-    $data = <<<DATA
-{
-  "name": "$name",
-  "round_id": "$round_id",
-  "puzzle_uri": "$puzzle_uri"
-}
-DATA;
+    $data = json_encode(array(
+        'name' => $name,
+        'round_id' => $round_id,
+        'puzzle_uri' => $puzzle_uri,
+    ));
     
     echo "<br> Submitting API request to add puzzle.  May take a few seconds.<br>";
     $resp = postapi($apiurl, $data);
@@ -37,7 +33,7 @@ DATA;
     }
     foreach($responseobj as $key => $value){
         if ($key == "status") {
-            if ($responseobj -> status == "ok") {
+            if ($responseobj->status == "ok") {
                 echo 'OK.  Puzzle created with ID of ' . $responseobj->puzzle->id;
             }
             else {
@@ -48,38 +44,40 @@ DATA;
             echo 'ERROR: ' . $value; 
         }
     }
+    die();
 }
 
-else {
-    
-    $puzzurl = "";
-    $puzzid = "";
-    if (isset( $_GET['puzzurl'])) {
-        $puzzurl = $_GET['puzzurl'];
-    }
-    if (isset( $_GET['puzzid'])) {
-        $puzzid = $_GET['puzzid'];
-    }
-        
-    $resp = readapi("/rounds");
-    $rounds = json_decode($resp)->rounds;
-    echo 'Add a puzzle!';
-    echo '<form action="addpuzzle.php" method="get">';
-    
-    echo 'Name:<input type = "text" name="name" required value="' . $puzzid . '" /><br>';
-    
-    echo 'Round:<select id="round_id" name="round_id"/>';
-    foreach($rounds as $round){
-        echo '<option value="' . $round->id . '">' . $round->name . '</option>';
-    }
-    echo '</select><br>';
-    
-    echo 'Puzzle URI:<input type = "text" name="puzzle_uri" required value="' . $puzzurl . '" /><br>';
-    
-    echo '<input type = "submit" name="submit" />';
-    echo '</form>';
+$puzzurl = "";
+$puzzid = "";
+if (isset($_GET['puzzurl'])) {
+    $puzzurl = $_GET['puzzurl'];
+}
+if (isset($_GET['puzzid'])) {
+    $puzzid = $_GET['puzzid'];
 }
 
+$resp = readapi("/rounds");
+$rounds = json_decode($resp)->rounds;
 ?>
+
+Add a puzzle!
+<form action="addpuzzle.php" method="get">
+Name:<input type="text" name="name" required value="<?= $puzzid ?>" />
+<br>
+
+Round:<select id="round_id" name="round_id"/>
+<?
+foreach ($rounds as $round) {
+    echo '<option value="' . $round->id . '">' . $round->name . '</option>';
+}
+?>
+</select><br>
+
+Puzzle URI:
+<input type="text" name="puzzle_uri" required value="<?= $puzzurl ?>" />
+<br>
+
+<input type="submit" name="submit" />
+</form>
 </body>
 </html>
