076a9ab4e0b4d0926e49ffcd341c9ead9da9228a
Danny Ben-David <dannybd>
2024-01-10 08:05:31 -0500

    Reformat and re-style /account

--- account/index.php
+++ account/index.php
@@ -3,13 +3,61 @@
 <head>
   <meta charset="UTF-8">
   <title>Puzzleboss Accounts</title>
+  <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&amp;family=Open+Sans:wght@400;700&amp;display=swap" rel="stylesheet">
+  <style>
+    body {
+      background-color: aliceblue;
+      display: grid;
+      font-family: 'Lora';
+      height: 100vh;
+      justify-items: center;
+      margin: 0;
+      width: 100vw;
+    }
+    h1 {
+      line-height: 1em;
+    }
+    h1 > span {
+      font-size: 50%;
+    }
+    main {
+      margin-top: 50px;
+      max-width: 700px;
+    }
+    table.registration {
+      text-align: right;
+    }
+    table.registration tr > td:last-child {
+      text-align: left;
+      font-size: 80%;
+      font-style: italic;
+    }
+    table.registration tr:last-child {
+      text-align: center;
+    }
+    input[type="submit"] {
+      font-family: inherit;
+    }
+    .error {
+      background-color: lightpink;
+      padding: 10px;
+    }
+  </style>
 </head>
 <body>
+<main>
 <?php
 global $apiroot;
 
 // TODO: Load this from the yaml config
+if ($_GET['yaml']) {
+  $yaml = yaml_parse_file('../puzzleboss.yaml');
+  print json_encode($yaml);
+  exit(0);
+}
 $apiroot = "http://localhost:5000";
+$google_domain = 'importanthuntpoll.org';
+$example_google_sheet_url = 'https://docs.google.com/spreadsheets/d/1l3Sgk-5XfCEMs4SKCn4uRfx5xagNbx1L-QvsQFSuhDU/edit';
 
 function readapi($apicall) {
   $url  = $GLOBALS['apiroot'] . $apicall;
@@ -43,151 +91,194 @@ function postapi($apicall, $data) {
   return json_decode($resp);
 }
 
+function exit_with_error_message($error) {
+  print <<<HTML
+    <div class="error">
+      <strong>ERROR:</strong>
+      &nbsp;$error;&nbsp;
+      <a href="javascript:window.history.back();">Try again.</a>
+    </div>
+  </main></body></html>
+HTML;
+  exit(0);
+}
+
+function exit_with_api_error($obj) {
+  exit_with_error_message(
+    'Response from API is:<pre>'.var_dump($obj).'</pre>'.
+    'Contact @Puzztech on Discord for help.'
+  );
+}
+
+function assert_api_success($responseobj) {
+  if (!$responseobj) {
+    exit_with_api_error($responseobj);
+  }
+  if (array_key_exists('error', $responseobj)) {
+    exit_with_api_error($responseobj->error);
+  }
+  if (!array_key_exists('status', $responseobj)) {
+    exit_with_api_error($responseobj);
+  }
+  if ($responseobj->status !== 'ok') {
+    exit_with_api_error($responseobj);
+  }
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  if (array_key_exists("userok", $_POST)) {
-    // user says it's ok let's get the code and display it
-    $data = array(
-      'username' => $_POST['username'],
-      'password' => $_POST['password'],
-      'fullname' => $_POST['fullname'],
-      'email' => $_POST['email'],
-      'reset' => $_POST['reset']
-    );
-    print "<h2>Submitting new user request to puzzleboss...</h2>";
-    print "<br>";
-    $responseobj = postapi('/account', $data);
-    echo '<br>';
-    if (!$responseobj) {
-      echo 'ERROR: Response from API is: <br><br>' . var_dump($responseobj);
-      echo '</body></html>';
-      exit(1);
-    }
-    foreach ($responseobj as $key => $value) {
-      if ($key == "status") {
-        if ($responseobj->status == "ok") {
-          echo 'OK.  Check your email from puzzleboss for further instructions.';
-        } else {
-          echo 'ERROR: Response from API is ' . var_dump($responseobj);
-        }
-      }
-      if ($key == "error") {
-        echo 'ERROR: ' . $value;
-      }
-    }
-
-    print "</body></html>";
-    exit(0);
-
-
+  $username = $_POST['username'];
+  $fullname = rtrim($_POST['fullname']);
+  $email    = $_POST['email'];
+  $password = $_POST['password'];
+  if (array_key_exists("reset", $_POST)) {
+    $reset = $_POST['reset'];
   } else {
+    $reset = "false";
+  }
+  $request_type = $reset !== 'false' ? 'account creation' : 'password reset';
+
+  // Run validation
+  if ($password != $_POST['password2']) {
+    exit_with_error_message("Passwords don't match");
+  }
+  if (!ctype_alnum($username)) {
+    exit_with_error_message("Username has non alphanumeric chars");
+  }
+  if (!preg_match('/^[a-zA-Z]+ [a-zA-Z]+$/', $fullname)) {
+    exit_with_error_message(
+      "Fullname must be <tt>Firstname Lastname</tt>, no non-alpha characters",
+    );
+  }
+  if (!preg_match('/@/', $email)) {
+    // I'm guessing we can do better email address validation here
+    exit_with_error_message("Valid email address required");
+  }
+  // End validation
+
+  if (!array_key_exists("userok", $_POST)) {
     // no code, but user data. present the page to submit user for verification
-    print "<h1>Verify provided information before submitting request</h1>";
-    print "<br>";
-
-    if ($_POST['password'] != $_POST['password2']) {
-      print "ERROR: Passwords don't match. Try again.";
-      print "</body></html>";
-      exit(0);
-    }
-
-    if (!ctype_alnum($_POST['username'])) {
-      print "ERROR: Username has non alphanumeric chars. Try again.";
-      print "</body></html>";
-      exit(0);
-    }
-
-    $fullname = rtrim($_POST['fullname']);
-
-    if (!preg_match('/^[a-zA-Z]+ [a-zA-Z]+$/', $fullname)) {
-      print "ERROR: Fullname must be Firstname Lastname, no non-alpha characters. Try again.";
-      print "</body></html>";
-      exit(0);
-
-    }
-
-    if (!preg_match('/@/', $_POST['email'])) {
-      // I'm guessing we can do better email address validation here
-      print "ERROR: Valid email address required";
-      print "</body></html>";
-      exit(0);
-    }
-    $username = $_POST['username'];
-    $email    = $_POST['email'];
-    $password = $_POST['password'];
-    if (array_key_exists("reset", $_POST)) {
-      $reset = $_POST['reset'];
-    } else {
-      $reset = "false";
-    }
-
-    $data = <<<DATA
-       <table>
-        <tr><td>Username:</td><td>$username</td></tr>
-        <tr><td>Fullname:</td><td>$fullname</td></tr>
-        <tr><td>Email:</td><td>$email</td></tr>
-        </table><br><hr><br>
-        Verify the above information.
-        If correct, submit account request to the system by pressing the "submit" button.<br>
-        Verification code and instructions to finish the process will be sent to<br>
-        the email address provided.
-        <form action="." method="POST">
+    print <<<HTML
+      <h1>Confirm $request_type details</h1>
+      <table>
+        <tr><td>Username:</td><td><tt>$username</tt></td></tr>
+        <tr><td>Full Name:</td><td><tt>$fullname</tt></td></tr>
+        <tr><td>Email:</td><td><tt>$email</tt></td></tr>
+      </table>
+      <br><hr>
+      <form action="." method="POST">
         <input type="hidden" name="username" value="$username">
         <input type="hidden" name="fullname" value="$fullname">
         <input type="hidden" name="password" value="$password">
         <input type="hidden" name="email" value="$email">
         <input type="hidden" name="userok" value="yes">
         <input type="hidden" name="reset" value="$reset">
-        <input type="submit" name="submit" value="submit">
-        </form>
-DATA;
-
-
-    print $data;
-    print "</body></html>";
+        <p>
+          If the above information looks correct, click here:&nbsp;
+          <input type="submit" name="submit" value="Confirm"><br>
+          We'll send you a confirmation code to the email you provided.
+        </p>
+        <p>
+          Need to change something?
+          <a href="javascript:window.history.back();">Go back</a> to try again.
+        </p>
+      </form>
+      </main></body></html>
+HTML;
     exit(0);
   }
-} else if (isset($_GET['code'])) {
+
+  // user says it's ok let's get the code and display it
+  try {
+    $responseobj = postapi(
+      '/account',
+      array(
+        'username' => $username,
+        'password' => $password,
+        'fullname' => $fullname,
+        'email' => $email,
+        'reset' => $reset
+      )
+    );
+  catch (Exception $e) {
+    exit_with_api_error($e);
+    throw $e;
+  }
+  assert_api_success($responseobj);
+  print <<<HTML
+  <h2>Request for $request_type submitted!</h2>
+  <p>Check your email ($email) for further instructions.</p>
+  </main></body></html>
+HTML;
+  exit(0);
+}
+
+if (isset($_GET['code'])) {
   print "<h2>Verifying.... code " . $_GET['code'] . "</h2>";
 
   $code = $_GET['code'];
 
-  print "<h2>Submitting new user finalization request to puzzleboss...</h2>";
-  print "<br>";
-  $responseobj = readapi('/finishaccount/' . $code);
-  echo '<br>';
-  if (!$responseobj) {
-    echo 'ERROR: Response from API is: <br><br>' . var_dump($responseobj);
-    echo '</body></html>';
-    exit(1);
+  try {
+    $responseobj = readapi('/finishaccount/' . $code);
+  } catch (Exception $e) {
+    exit_with_api_error($e);
+    throw $e;
   }
-  foreach ($responseobj as $key => $value) {
-    if ($key == "status") {
-      if ($responseobj->status == "ok") {
-        echo 'OK.  User has been created.  All hunt tools accessible. <br><br>';
-        echo "You should now be able to log in at: <a href='../'>Main Hunt Wiki</a><br><br>";
-        echo "If you encounter any difficulties, ask in the server-help channel in discord.";
-      } else {
-        echo 'ERROR: Response from API is ' . var_dump($responseobj);
-      }
-    }
-    if ($key == "error") {
-      echo 'ERROR: ' . $value;
-    }
-  }
-
-
-
-  print "</body></html>";
+  assert_api_success($responseobj);
+  print <<<HTML
+  <h2>Account creation successful!</h2>
+  <p>
+    Your account on Puzzleboss has been created/updated.
+    All our hunt tools should now accessible.
+  </p>
+  <p>Please confirm that's the case by:</p>
+  <ol>
+    <li>Logging into our <a href="/" target="_blank">Team Wiki</a> with the username/password you just made</li>
+    <li>
+      Loading
+      <a href="$example_google_sheet_url" target="_blank">this test Google Spreadsheet</a>.
+      <ul>
+        <li>
+          Use <tt>YOUR_USERNAME@$google_domain</tt> and your $google_domain password to log into Google.
+        </li>
+        <li>
+          (Consider creating
+          <a href="https://support.google.com/chrome/answer/2364824" target="_blank">a new Chrome profile</a>
+          for Mystery Hunt; it also helps quarantine your wild Hunt search history.)
+        </li>
+      </ul>
+    </li>
+    <li>
+      Returning to our Discord and ping @RoleVerifier with your username
+      so we can connect your Puzzleboss and Discord accounts.
+    </li>
+  </ol>
+  <p>
+  If you hit any other issues, ping @Puzztech in the Discord.<br>
+  Thanks, and get excited to Hunt!
+  </p>
+  </main></body></html>
+HTML;
   exit(0);
 }
 
 ?>
-<h1>Puzzleboss 2000 New Account Registration (or password reset)</h1>
+<h1>
+  Puzzleboss 2000 New Account Registration<br>
+  <span>(or password reset)</span>
+</h1>
+<p>
+  Welcome aboard! <em>Puzzleboss</em> is our team's puzzlesolving infrastructure:
+  it helps us track which puzzles we have open, create spreadsheets for each of them,
+  and know who's working on what. <strong>You'll need an account to hunt with us.</strong>
+</p>
+<p>
+  If you're a new solver, welcome! If you're returning, you only need to use this if you've forgotten your password. (If so, check the password reset box as you fill this out.)
+</p>
 <form action="?" method="POST">
-<table style="text-align:right;">
+<table class="registration">
   <tr>
     <td><label for="username">Username (alphanumeric only, max 20 chars):</label></td>
-    <td><input type="text" id="username" name="username" required /></td>
+    <td><input type="text" id="username" name="username" pattern="[A-Za-z0-9]+" required /></td>
   </tr>
   <tr>
     <td><label for="fullname">Full Name (alpha only Firstname Lastname):</label></td>
@@ -227,6 +318,13 @@ DATA;
     <td><label for="reset">Check box if this is a password reset:</label></td>
     <td><input type="checkbox" id="reset" name="reset" value="reset"></td>
   </tr>
+  <tr>
+    <td />
+    <td><input type="submit" name="submit" value="Submit"></td>
+    <td />
+  </tr>
 </table>
-<input type="submit" name="submit" value="Submit">
 </form>
+</main>
+</body>
+</html>
