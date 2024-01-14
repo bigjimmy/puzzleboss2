21e5b605237c739eb6d9f00e840dd7febda64834
Danny Ben-David <dannybd>
2024-01-13 19:29:52 -0500

    merge bookmarklets

--- www/pbtools.php
+++ www/pbtools.php
@@ -1,23 +1,10 @@
 <?php
 require('puzzlebosslib.php');
 
-$newpuzzle_bookmarkuri = <<<'JAVASCRIPT'
-javascript:window.open((data => `<<<ADDPUZZLEURI>>>?puzzurl=${encodeURIComponent(location.href.split('#')[0])}&puzzid=${encodeURIComponent(data?.name || document.title.replace(/ - Google Docs$/, ''))}&roundname=${encodeURIComponent(data?.round?.name?.replace(/[^A-Za-z0-9]+/g, ''))}`)(JSON.parse(document.querySelector('#__NEXT_DATA__')?.innerText || '{}')?.props?.pageProps?.puzzleData));
+$bookmarkuri = <<<'JAVASCRIPT'
+javascript:if(window.location.pathname==='/puzzles'){pbPath=(data => '?'+Object.entries(data.puzzles || {}).map(([roundSlug, puzzles]) => ['r[' + data.rounds[roundSlug].name.replace(/[^A-Za-z0-9]/g,'') + ']', puzzles.filter(({answer}) => !answer).map(({slug, isMeta}) => (isMeta ? '!' : '') + slug).join(',')].join('=')).join('&'))(JSON.parse(document.getElementById('__NEXT_DATA__')?.innerText || '{}')?.props?.pageProps || {});}else{pbPath=(data => `addpuzzle.php?puzzurl=${encodeURIComponent(location.href.split('#')[0])}&puzzid=${encodeURIComponent(data?.name || document.title.replace(/ - Google Docs$/, ''))}&roundname=${encodeURIComponent(data?.round?.name?.replace(/[^A-Za-z0-9]+/g, ''))}`)(JSON.parse(document.querySelector('#__NEXT_DATA__')?.innerText || '{}')?.props?.pageProps?.puzzleData);}window.open('<<<PBROOTURI>>>'+pbPath);
 JAVASCRIPT;
-$newpuzzle_bookmarkuri = trim(str_replace(
-  '<<<ADDPUZZLEURI>>>',
-  $pbroot.'addpuzzle.php',
-  $newpuzzle_bookmarkuri,
-));
-
-$compare_bookmarkuri = <<<'JAVASCRIPT'
-javascript:window.open('<<<PBROOTURI>>>?'+(data => Object.entries(data.puzzles || {}).map(([roundSlug, puzzles]) => ['r[' + data.rounds[roundSlug].name.replace(/[^A-Za-z0-9]/g,'') + ']', puzzles.filter(({answer}) => !answer).map(({slug, isMeta}) => (isMeta ? '!' : '') + slug).join(',')].join('=')).join('&'))(JSON.parse(document.getElementById('__NEXT_DATA__')?.innerText || '{}')?.props?.pageProps || {}));
-JAVASCRIPT;
-$compare_bookmarkuri = trim(str_replace(
-  '<<<PBROOTURI>>>',
-  $pbroot,
-  $compare_bookmarkuri,
-));
+$bookmarkuri = trim(str_replace('<<<PBROOTURI>>>', $pbroot, $bookmarkuri));
 
 ?>
 <!doctype html>
@@ -71,29 +58,20 @@ $compare_bookmarkuri = trim(str_replace(
 <h1>Puzzleboss-only Admin Tools</h1>
 
 <hr>
-<h3>New Puzzle Bookmarklet</h3>
+<h3>New Puzzle / Check for Puzzles Bookmarklet</h3>
+A major timesaver for Puzzlebosses, this bookmarklet works in two ways:
+<ul>
+  <li>On a puzzle page, click it to create a new puzzle</li>
+  <li>On the List of Puzzles page (<tt>/puzzles</tt>), click it to check if PB is missing anything.</li>
+</ul>
 <table border="2" cellpadding="3">
   <tr>
-    <td>For a bookmark for adding a new puzzle (while on a puzzle page), drag this link to your bookmarks:</td>
-    <td><a href="<?= $newpuzzle_bookmarkuri ?>">Puzzleboss New Puzzle</a></td>
+    <td>Drag this link to your bookmarks:</td>
+    <td><a href="<?= $bookmarkuri ?>">Puzzleboss New/Check Puzzles</a></td>
   </tr>
   <tr>
     <td>Or alternatively, copy the following into a new bookmark:</td>
-    <td><code><?= $newpuzzle_bookmarkuri ?></code></td>
-  </tr>
-</table>
-<br>
-
-<hr>
-<h3>Compare Puzzles Bookmarklet</h3>
-<table border="2" cellpadding="3">
-  <tr>
-    <td>For a bookmark for checking if there are new puzzles (from the <tt>/puzzles</tt> page, drag this link to your bookmarks:</td>
-    <td><a href="<?= $compare_bookmarkuri ?>">Puzzleboss Check New Puzzles</a></td>
-  </tr>
-  <tr>
-    <td>Or alternatively, copy the following into a new bookmark:</td>
-    <td><code><?= $compare_bookmarkuri ?></code></td>
+    <td><pre style="text-size: 40%;"><?= $bookmarkuri ?></pre></td>
   </tr>
 </table>
 <br>
