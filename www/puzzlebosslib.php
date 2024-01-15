43eb602694fb5783131339294fe2ef20a4ef0cdd
Danny Ben-David <dannybd>
2024-01-15 00:30:25 -0500

    fix

--- www/puzzlebosslib.php
+++ www/puzzlebosslib.php
@@ -119,8 +119,9 @@ function getauthenticateduser() {
     http_response_code(403);
     die("No solver found for user $username. Check Solvers Database");
   }
-  $debugging_usernames = explode(',', idx($config, 'debugging_usernames', ''));
-  if (in_array($username, $debugging_usernames)) {
+  // $debugging_usernames = explode(',', idx($config, 'debugging_usernames', ''));
+  // if (in_array($username, $debugging_usernames)) {
+  if ($username == 'dannybd')
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
   }
