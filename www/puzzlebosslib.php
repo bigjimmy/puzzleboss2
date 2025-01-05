c57ae13fef6b4e89398bd008b8ccd364fa5a889a
Benjamin O'Connor <benoc@alum.mit.edu>
2025-01-05 05:56:07 +0000

    cleanup

--- www/puzzlebosslib.php
+++ www/puzzlebosslib.php
@@ -5,15 +5,14 @@ global $pbroot;
 global $bookmarkuri;
 
 $yaml = yaml_parse_file('../puzzleboss.yaml');
+$pbroot = $yaml['APP']['BIN_URI'];
 $apiroot = $yaml['BIGJIMMYBOT']['APIURI'];
 $phproot = "http://localhost:8080/puzzleboss/www/";
 $noremoteusertestmode = "true"; //TODO: eliminate this
 
 //TODO: add error handling here for mandatory config values. should direct user to admin page for config editing.
 $config = readapi('/config')->config;
-$pbroot = $config->BIN_URI;
 $bookmarkuri = $config->bookmarklet_js;
-
 
 function readapi($apicall) {
   $url = $GLOBALS['apiroot'] . $apicall;
