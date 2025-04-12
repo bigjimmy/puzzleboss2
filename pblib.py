aa4ee98512fba221f51d08d511963f1ba769be05
Benjamin O'Connor <benoc@alum.mit.edu>
2025-04-12 16:56:12 +0000

    Revert "fixes for real this time"
    This reverts commit 8444a9fa8debfce2d498738beea570e42c7700ac.

--- pblib.py
+++ pblib.py
@@ -5,6 +5,7 @@ import bleach
 import smtplib
 import MySQLdb
 from flask import Flask, request
+from flask_restful import Api
 from flask_mysqldb import MySQL
 from email.message import EmailMessage
 
